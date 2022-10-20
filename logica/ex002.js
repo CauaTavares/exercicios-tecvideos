4f957600c74d2076bc143284988efb8d29937eab
CauaTavares <cauaxd2010@outlook.com>
2022-10-19 21:24:45 -0300

    finalizei o ex002

---
+++ logica/ex002.js
@@ -0,0 +1,10 @@
+let valorJantar = 80; 
+let taxaGarcom = 10;
+let total = 0;
+
+total = 80 + ((taxaGarcom / 100) * 80);
+
+// console.log('Valor do jantar: R$' + valroJantar + ',00');
+console.log(`Valor do jantar: R$${valorJantar},00`);
+console.log(`Taxa do garcom: R$${(taxaGarcom / 100) * 80},00`);
+console.log(`Total a pagar: R$${total},00`);
