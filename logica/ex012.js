ccc8e0f8f550e31aeb4fd8813961b3a3568bba29
CauaTavares <cauaxd2010@outlook.com>
2022-11-02 14:28:49 -0300

    finalizei o ex012

---
+++ logica/ex012.js
@@ -0,0 +1,10 @@
+let numero = 13;
+
+// if(numero % 2 === 0) {
+//     console.log(`O número ${numero} é PAR!`);
+// } else {
+//     console.log(`O número ${numero} é IMPAR!`);
+// }
+
+
+(numero % 2 === 0) ? console.log(`O número ${numero} é PAR!`) : console.log(`O número ${numero} é IMPAR!`);
