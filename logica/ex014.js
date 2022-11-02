0fe550809310eca3c8ae27b7b15174e126c864d7
CauaTavares <cauaxd2010@outlook.com>
2022-11-02 15:05:55 -0300

    finalizei o ex014

---
+++ logica/ex014.js
@@ -0,0 +1,13 @@
+let horaBrasil = 22;
+let minutos = 28;
+let horaFranca = horaBrasil + 5;
+
+// if(horaFranca > 24) {
+//     horaFranca = horaFranca - 24;
+// }
+
+horaFranca > 24 ? horaFranca = horaFranca - 24 : horaFranca
+
+
+console.log(`Brasil: ${horaBrasil}:${minutos} 
+Franca: ${horaFranca}:${minutos}`);
