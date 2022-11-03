a2b1f381636d7f310af1fbc3baf584acd11b5dc7
CauaTavares <cauaxd2010@outlook.com>
2022-11-03 18:42:52 -0300

    finalizei o ex016

--- logica/ex016.js
+++ logica/ex016.js
@@ -1,19 +1,19 @@
 let alturas = [];
 function criaPessoa (num) {
     for(let index = 0; index < num; index += 1){
-        alturas.push((Math.random() * (2.5 - 1.3) + 1.3).toFixed(2));
+        alturas.push((Math.random() * (2.5 - 1.3) + 1.3));
     }
 };
 
 criaPessoa(10);
 
-console.log(alturas);
+// console.log(alturas);
 // console.log((Math.random() * (2.5 - 1.3) + 1.3).toFixed(2));
 
 let soma = 0
 alturas.forEach((altura) => soma += altura);
 
-const media = (soma / alturas.length).toFixed(2);
+const media = (soma / alturas.length);
 
 // console.log(soma);
 
@@ -22,7 +22,7 @@ alturas.forEach((altura) => {
     if(altura < media) menorMedia += 1; 
 });
 
-console.log(`Menor altura: ${alturas.sort()[0]}`);  
-console.log(`Maior altura: ${(alturas.sort((a, b) => b - a)[0])}`);
-console.log(`Média: ${media}`);
+console.log(`Menor altura: ${(alturas.sort()[0]).toFixed(2)}`);  
+console.log(`Maior altura: ${(alturas.sort((a, b) => b - a)[0]).toFixed(2)}`);
+console.log(`Média: ${media.toFixed(2)}`);
 console.log(`Quantidade de pessoas com altura menor que a média ${menorMedia}`);
